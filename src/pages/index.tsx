54809b8dae0559ccc148c44b2a0b2c1da1111476
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-07-31 23:54:35 +0300

    feat: Add a basic topbar (#3)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import Topbar from "@/components/Topbar/Topbar";
 import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <main className="bg-dark-layer-2 min-h-screen">
-        <h1>Aloha World!</h1>
+        <Topbar />
       </main>
     </>
   );
