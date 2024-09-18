ce8b591a637e5a85fdefd3034324c944f0fc4d9d
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-09-18 14:05:55 +0300

    fix: Banner width and height

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -16,8 +16,7 @@ const Topbar: React.FC<TopbarProps> = () => {
           <Image
             src={Logo}
             alt="Logo"
-            width={360}
-            className="h-full absolute top-0 left-[-58px]"
+            className="h-full absolute top-0 left-[-58px] w-auto"
             priority
           />
         </Link>
