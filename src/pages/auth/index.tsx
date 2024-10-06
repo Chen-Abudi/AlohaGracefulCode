d5c004d37e301b44ac93cfe722f70c0536cd80c7
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-10-06 19:17:21 +0300

    Update/auth page (#9)
    * update: Add logo
    
    * chore: Clear comment

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilValue } from "recoil";
+import Image from "next/image";
+import Logo from "../../../public/logo-banner.png";
 
 type AuthPageProps = {};
 
@@ -27,7 +29,12 @@ const AuthPage: React.FC<AuthPageProps> = () => {
       <div className="max-w-7xl mx-auto">
         <NavBar />
         <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
-          {/* <img src="/hero.png" alt="Hero Image" /> */}
+          <Image
+            src={Logo}
+            alt="Homepage Logo"
+            className="absolute top-[-23px] left-11 w-auto"
+            priority
+          />
         </div>
         {authModal.isOpen && <AuthModal />}
       </div>
