1f708615454d26f60a57457e92b27dccaa42fd88
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-10-06 18:52:57 +0300

    feat: Add logout (#8)

---
+++ src/components/Buttons/Logout.tsx
@@ -0,0 +1,25 @@
+import { auth } from "@/firebase/firebase";
+import React from "react";
+import { useSignOut } from "react-firebase-hooks/auth";
+import { FiLogOut } from "react-icons/fi";
+
+type LogoutProps = {};
+
+const Logout: React.FC<LogoutProps> = () => {
+  const [signOut, loading, error] = useSignOut(auth);
+
+  const handleLogout = () => {
+    signOut();
+  };
+
+  return (
+    <button
+      className="bg-dark-fill-3 py-1.5 px-3 rounded text-brand-orange cursor-pointer"
+      onClick={handleLogout}
+    >
+      <FiLogOut />
+    </button>
+  );
+};
+
+export default Logout;
