4586b739384cbbb19e5f7328d014b2ffd9b87244
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-10-20 22:37:26 +0300

    feat: Add worspace (#13)

---
+++ src/components/Workspace/Workspace.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Split from "react-split";
+import ProblemDescription from "./ProblemDescription/ProblemDescription";
+
+type WorkspaceProps = {};
+
+const Workspace: React.FC<WorkspaceProps> = () => {
+  return (
+    <Split className="split" minSize={0}>
+      <ProblemDescription />
+      <div>The code editor will be here</div>
+    </Split>
+  );
+};
+
+export default Workspace;
