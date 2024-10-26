6e1eaaaf522a0487e983059b269b93e09502042f
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-10-26 20:53:38 +0300

    feat: Add playground (#14)

---
+++ src/components/Workspace/Playground/Playground.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PreferenceNavigation from "./PreferenceNavigation/PreferenceNavigation";
+
+type PlaygroundProps = {};
+
+const Playground: React.FC<PlaygroundProps> = () => {
+  return (
+    <>
+      <PreferenceNavigation />
+    </>
+  );
+};
+
+export default Playground;
