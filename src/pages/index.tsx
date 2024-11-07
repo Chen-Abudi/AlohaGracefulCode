70e47be28ee9a24d6d7b2788bdc63afdd2e08187
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-11-07 23:57:24 +0200

    chore: Clear comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,10 @@
 import React, { useState } from "react";
-// import { doc, setDoc } from "firebase/firestore";
-// import { firestore } from "@/firebase/firebase";
 
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
 
 export default function Home() {
   const [loadingProblems, setLoadingProblems] = useState(true);
-
-  // const [inputs, setInputs] = useState({
-  //   id: "",
-  //   title: "",
-  //   difficulty: "",
-  //   category: "",
-  //   order: 0,
-  //   videoId: "",
-  //   link: "",
-  //   likes: 0,
-  //   dislikes: 0,
-  // });
-
-  // const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-  //   setInputs({
-  //     ...inputs,
-  //     [evt.target.name]: evt.target.value,
-  //   });
-  // };
-
-  // const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
-  //   evt.preventDefault();
-  //   // Convert inputs.order into an integer
-  //   const newProblem = {
-  //     ...inputs,
-  //     order: Number(inputs.order),
-  //   };
-  //   await setDoc(doc(firestore, "problems", inputs.id), newProblem);
-  //   alert("Save to DB");
-  // };
 
   return (
     <>
@@ -76,78 +44,9 @@ export default function Home() {
                 </tr>
               </thead>
             )}
-            {/* <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b">
-              <tr>
-                <th className="px-1 py-3 w-0 font-medium" scope="col">
-                  Status
-                </th>
-                <th className="px-6 py-3 w-0 font-medium" scope="col">
-                  Title
-                </th>
-                <th className="px-6 py-3 w-0 font-medium" scope="col">
-                  Difficulty
-                </th>
-                <th className="px-6 py-3 w-0 font-medium" scope="col">
-                  Category
-                </th>
-                <th className="px-6 py-3 w-0 font-medium" scope="col">
-                  Solution
-                </th>
-              </tr>
-            </thead> */}
             <ProblemsTable setLoadingProblems={setLoadingProblems} />
           </table>
         </div>
-
-        {/* Temporary Form */}
-        {/* <form
-          className="flex flex-col gap-3 max-w-sm p-6"
-          onSubmit={handleSubmit}
-        >
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="problem id"
-            name="id"
-          />
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="title"
-            name="title"
-          />
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="difficulty"
-            name="difficulty"
-          />
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="category"
-            name="category"
-          />
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="order"
-            name="order"
-          />
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="videoId?"
-            name="videoId"
-          />
-          <input
-            onChange={handleInputChange}
-            type="text"
-            placeholder="link?"
-            name="link"
-          />
-          <button className="bg-white">Save to DB</button>
-        </form> */}
       </main>
     </>
   );
