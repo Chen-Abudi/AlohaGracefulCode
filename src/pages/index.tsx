693635170e183e3000ac2d169ccd538eb07c7e20
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-12-03 18:25:19 +0200

    fix: Fix hydration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
+import useHasMounted from "@/hooks/useHasMounted";
 
 export default function Home() {
   const [loadingProblems, setLoadingProblems] = useState(true);
+  const hasMounted = useHasMounted;
+
+  if (!hasMounted) return null;
 
   return (
     <>
